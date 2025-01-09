2fe762ef06c31ab3f7e70d687665ac8461ba985a
afandy110 <148042060+afandy110@users.noreply.github.com>
2025-01-09 07:17:43 +0700

    Init

---
+++ dart/add.dart
@@ -0,0 +1,25 @@
+import 'dart:ffi';
+
+typedef Cfunc = Int32 Function(Int32, Int32);
+typedef Dartfunc = int Function(int, int);
+
+int main() {
+  print("Opening.....");
+  final lib = DynamicLibrary.open(
+      '../bin/Release/net8.0/linux-x64/publish/NativeLibrary.so');
+
+  print("Associating.....");
+  Pointer<NativeFunction<Cfunc>> cadd =
+      lib.lookup<NativeFunction<Cfunc>>('aotsample_add');
+  Dartfunc add = cadd.asFunction<Dartfunc>();
+
+  print("Calling.....");
+  final result = add(5, 3);
+  if (result == 8) {
+    print("Hoorah! the result is $result, this is correct");
+  } else {
+    print("Oops something is wrong the result is $result");
+  }
+
+  return 0;
+}
